002dd4971cbaeffafe94904a227ff8b0e84ca211
Alan Christianson <git@c10n.net>
2014-12-11 16:54:54 -0700

    Add scripts for OSX
    Add scripts for setting up an OSX system, including
    installing software via Homebrew.

---
+++ osx/brewInstall.sh
@@ -0,0 +1,17 @@
+#cmd line tools
+brew install redis
+brew install wget
+
+#install cask
+brew install caskroom/cask/brew-cask
+
+#apps
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install firefox
+brew cask install dropbox
+brew cask install macvim
+brew cask install adium
+brew cask install flash
+
+#python? sublime-text?databases?
---
+++ osx/macSetup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# install all the software in Brewfile using homebrew/cask
+/bin/bash ./brewInstall.sh
+
+#set up defaults
