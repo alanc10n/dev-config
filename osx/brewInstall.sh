aa0c832699af2bf836a0d5f72dcaf8c56221b440
Alan Christianson <git@c10n.net>
2014-12-12 07:57:56 -0700

    Install MacVim via cask
    Continue installing MacVim via cask, as the problem is with
    Spotlight not indexing through symlinks. This is suboptimal, but
    an alias can be added in /Applications to work around it for now.

--- osx/brewInstall.sh
+++ osx/brewInstall.sh
@@ -10,9 +10,7 @@ brew cask install virtualbox
 brew cask install vagrant
 brew cask install firefox
 brew cask install dropbox
-# couldn't get it to both be indexed by Spotlight and launchable
-# via Terminal...need to fix mvim script or install differently
-#brew cask install macvim
+brew cask install macvim
 
 brew cask install adium
 brew cask install flash
