50724008372c3c611f4461e8db0ffe3ec1677dc5
Alan Christianson <git@c10n.net>
2014-12-27 15:14:04 -0700

    Set up global git ignore file

--- git/gitSetup.sh
+++ git/gitSetup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+SRC_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 #set name and email
 git config --global user.name "Alan Christianson"
 git config --global user.email "git@c10n.net"
 
+# set up global gitignore
+ln -s $SRC_DIR/gitignore_global ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
 
