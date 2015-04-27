cab2e1e991143ca41ae4dcddc1f743d8fb87c227
Alan Christianson <git@c10n.net>
2015-04-27 10:36:36 -0600

    Fix bash setup for Linux

--- bash/bashSetup.sh
+++ bash/bashSetup.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 SRC_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-ln -s $SRC_DIR/bash_profile ~/.bash_profile
+
+# on OS X we can use .bash_profile, but on Linux desktops we'll
+# want to use .profile instead (for a graphical, non-login shell)
+
+OS=$(uname -s)
+DEST=""
+if [ "$OS" = "Darwin" ]; then
+    DEST="${HOME}/.bash_profile"
+elif [ "$OS" = "Linux" ]; then
+    DEST="${HOME}/.profile"
+else
+    echo "Error: Unknown OS: $OS"
+    return 1
+fi
+
+#check if dest exists so we don't blow away existing config
+if [ -e $DEST ]; then
+    cat "${SRC_DIR}/bash_profile" >> $DEST
+else
+    ln -s "${SRC_DIR}/bash_profile" $DEST
+fi
+
+echo "Log out and in again to apply bash profile"
